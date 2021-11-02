b55430cbacb4cd6a2c5a1622920e147a4803b880
Maxime Jezequel <maxime.jezequel@gmail.com>
2021-11-02 17:03:06 +0100

    Simplified db-config.js and removed copy of App.js

--- db-config.js
+++ db-config.js
@@ -1,19 +1,13 @@
 require("dotenv").config()
 const { MongoClient } = require("mongodb")
 
-// Declare variables
-const db_user = process.env.DB_USER
-const db_password = process.env.DB_PASSWORD
-const db_host = process.env.DB_HOST
-const db_name = process.env.DB_NAME
-
 // MongoDB Url & parameters
 const connectionParams = {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 }
 
-const atlas_url = `mongodb+srv://${db_user}:${db_password}@${db_host}/${db_name}?retryWrites=true&w=majority`
+const atlas_url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`
 const client = new MongoClient(atlas_url, connectionParams)
 
 module.exports = client
